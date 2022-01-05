c85118d434b65c3acf4ef125667be325e9ee9ed9
23jonathan23 <jonathanraphael74@outlook.com>
2022-01-04 22:53:55 -0300

    table pages added for process

--- src/application/domain/Process.java
+++ src/application/domain/Process.java
@@ -1,15 +1,25 @@
 package application.domain;
+
+import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 public class Process implements Comparable<Process> {
     private UUID pid;
     private int priority;
     private ProcessState state;
+    private TablePages tablePages;
+    private int sizeInKiloBytes;
+    private Random random;
+    public static int limitPages = 40;
 
-    public Process(int priority) {
+    public Process(int priority, int sizeInKiloBytes) {
         this.pid = UUID.randomUUID();
         this.priority = priority;
         this.state = ProcessState.NEW;
+        this.sizeInKiloBytes = sizeInKiloBytes;
+        this.tablePages = new TablePages(limitPages);
+        this.random = new Random();
     }
 
     public UUID getPid() {
@@ -24,12 +34,30 @@ public class Process implements Comparable<Process> {
         return state;
     }
 
+    public int getSize() {
+        return sizeInKiloBytes;
+    }
+
+    public UUID getRandomPageId() {
+        var indexRandom = random.nextInt(this.tablePages.getTotalPages());
+
+        return this.tablePages.getPageId(indexRandom);
+    }
+
+    public List<UUID> getCurrentPagesId() {
+        return this.tablePages.getPagesId();
+    }
+
     public void setPriority(int priority) {
         this.priority = priority;
     }
 
     public void setState(ProcessState state) {
         this.state = state;
+    }
+
+    public void addPageId(UUID pageId) {
+        this.tablePages.addPageId(pageId);
     }
 
     @Override
